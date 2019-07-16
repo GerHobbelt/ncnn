f77504445aa3aea2376f5eaf91b07e837269cfd3
Thugather Dios <vocalus0001@gmail.com>
2019-07-16 19:04:20 +0800

    itobiu

---
+++ zbu.bat
@@ -0,0 +1,8 @@
+cd %APPVEYOR_BUILD_FOLDER%\ap\build-vs2017
+cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ../cmake
+nmake
+nmake install
+cd %APPVEYOR_BUILD_FOLDER%\build-vs2017
+cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -DProtobuf_INCLUDE_DIR=<protobuf-root-dir>/build-vs2017/install/include -DProtobuf_LIBRARIES=<protobuf-root-dir>/build-vs2017/install/lib/libprotobuf.lib -DProtobuf_PROTOC_EXECUTABLE=<protobuf-root-dir>/build-vs2017/install/bin/protoc.exe ..
+nmake
+nmake install
