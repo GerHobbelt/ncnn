07ceed6b9d23afc7c0c2f654bdd615af93484f0d
Klanly <natural858@gmail.com>
2019-07-16 20:49:28 +0800

    Update zbu2.bat

--- zbu2.bat
+++ zbu2.bat
@@ -6,7 +6,7 @@ cd %APPVEYOR_BUILD_FOLDER%
 md build-vs2017
 cd build-vs2017
 
-cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -DProtobuf_INCLUDE_DIR==%APPVEYOR_BUILD_FOLDER%/ap//build-vs2017/install/include -DProtobuf_LIBRARIES==%APPVEYOR_BUILD_FOLDER%/ap//build-vs2017/install/lib/libprotobuf.lib -DProtobuf_PROTOC_EXECUTABLE==%APPVEYOR_BUILD_FOLDER%/ap//build-vs2017/install/bin/protoc.exe -DNCNN_VULKAN=ON ..
+cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -DProtobuf_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/include -DProtobuf_LIBRARIES=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/lib/libprotobuf.lib -DProtobuf_PROTOC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/bin/protoc.exe -DNCNN_VULKAN=ON ..
 
 
 nmake
