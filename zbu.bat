3f1eb0a1cdcdc467dfdf52af554cfacb3e0e6511
Klanly <natural858@gmail.com>
2019-07-16 19:56:56 +0800

    Update zbu.bat

--- zbu.bat
+++ zbu.bat
@@ -5,11 +5,4 @@ cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%
 nmake
 nmake install
 
-echo buildncc
 
-cd %APPVEYOR_BUILD_FOLDER%
-md build-vs2017
-cd build-vs2017
-cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -DProtobuf_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/include -DProtobuf_LIBRARIES=<protobuf-root-dir>/build-vs2017/install/lib/libprotobuf.lib -DProtobuf_PROTOC_EXECUTABLE=<protobuf-root-dir>/build-vs2017/install/bin/protoc.exe ..
-nmake
-nmake install
