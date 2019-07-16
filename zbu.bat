6e1782445f2e672c18b1e04e087e7c65e344db83
Klanly <natural858@gmail.com>
2019-07-16 19:27:57 +0800

    Update zbu.bat

--- zbu.bat
+++ zbu.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
 
 cd %APPVEYOR_BUILD_FOLDER%\ap\build-vs2017
 cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ../cmake
