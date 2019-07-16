fca90b832ddb0df41b0a1f7a0a948179812649b6
Klanly <natural858@gmail.com>
2019-07-16 20:26:31 +0800

    Update zbu.bat

--- zbu.bat
+++ zbu.bat
@@ -1,5 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64 10.0.10240.0
 
+appveyor DownloadFile https://vulkan.lunarg.com/sdk/download/1.1.108.0/windows/VulkanSDK-1.1.108.0-Installer.exe?Human=true -FileName VulkanSDK.exe
+VulkanSDK.exe /S
+
 cd %APPVEYOR_BUILD_FOLDER%\ap\build-vs2017
 cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_MSVC_STATIC_RUNTIME=OFF ../cmake
 nmake
