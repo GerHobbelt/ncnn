a67106665bbca16fd419172ed193d043ccb0f97e
Klanly <natural858@gmail.com>
2019-07-17 11:20:37 +0800

    Update zbu5.bat

--- zbu5.bat
+++ zbu5.bat
@@ -6,7 +6,7 @@ cd %APPVEYOR_BUILD_FOLDER%
 md build-vs2017
 cd build-vs2017
 
-cmake -G"Visual Studio 14 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -DProtobuf_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/include -DProtobuf_LIBRARIES=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/lib/libprotobuf.lib -DProtobuf_PROTOC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/bin/protoc.exe -DNCNN_VULKAN=ON ..
+cmake -G"Visual Studio 15 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -DProtobuf_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/include -DProtobuf_LIBRARIES=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/lib/libprotobuf.lib -DProtobuf_PROTOC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/bin/protoc.exe -DNCNN_VULKAN=ON ..
 
 
 
