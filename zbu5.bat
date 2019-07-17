b385f2f5f40263dd07512290d4188fb080b41245
Klanly <natural858@gmail.com>
2019-07-17 11:19:35 +0800

    Create zbu5.bat

---
+++ zbu5.bat
@@ -0,0 +1,15 @@
+echo buildncc
+
+
+
+cd %APPVEYOR_BUILD_FOLDER%
+md build-vs2017
+cd build-vs2017
+
+cmake -G"Visual Studio 14 2017 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install -DProtobuf_INCLUDE_DIR=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/include -DProtobuf_LIBRARIES=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/lib/libprotobuf.lib -DProtobuf_PROTOC_EXECUTABLE=%APPVEYOR_BUILD_FOLDER%/ap/build-vs2017/install/bin/protoc.exe -DNCNN_VULKAN=ON ..
+
+
+
+cd %APPVEYOR_BUILD_FOLDER%
+del VulkanSDK.exe
+rd /s /q ap
