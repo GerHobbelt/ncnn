9543d6cf405d8ee4c294cc92cf8db1b4c4e491ea
Klanly <natural858@gmail.com>
2021-03-19 20:40:12 +0800

    Add files via upload

---
+++ keepsec/kero/Program.cs
@@ -0,0 +1,129 @@
+﻿
+using System;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace eroneto
+{
+	
+	static class Program
+	{
+		static HashSet<string> deuu = new HashSet<string>();
+		static WebClient dndr = new WebClient();
+		
+		[DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+		public static extern int system(string command);
+		
+		public static char[] sep0xA={'\n'};
+		public static char[] sepQuo={'\"'};
+		public static char[] sep0x9={'\t'};
+		
+		static void klening()
+		{
+			string[] data=File.ReadAllLines("rlines.csv");
+			File.Move("rlines.csv","zb/rlines."+DateTimeOffset.Now.ToUnixTimeSeconds().ToString("X")+".csv");
+			
+			int cota=data.Length-deuu.Count;
+			
+			List<string> kle=new List<string>();
+			
+			for(int i=0;i<cota;i++)
+			{
+				var ivo=data[i].Split(sep0x9);
+				if(!deuu.Contains(ivo[0]))
+				{
+					kle.Add(data[i]);
+				}
+
+			}
+			
+			int cota2=data.Length;
+			for(int i=cota;i<cota2;i++)
+			{
+					kle.Add(data[i]);
+			}
+			
+			File.WriteAllLines("rlines.csv",kle.ToArray());
+		}
+		
+		static void Main(string[] args)
+		{
+			ServicePointManager.Expect100Continue = true;                
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+			ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+			StreamWriter sw = File.AppendText("rlines.csv");
+			while(true)
+			{
+			
+				string[] html=dndr.DownloadString("https://www.nurumayu.net/ko/twidouga/realtime_t.php").Split(sep0xA);
+				
+				int htmllen=html.Length;
+				bool getsome=false;
+				for(int i=0;i<htmllen;i++)
+				{
+					if(html[i].Length>200&&html[i].Contains("video.twimg.com"))
+					{
+						string[] siu=html[i].Split(sepQuo);
+						
+						string thupic=siu[7].Replace("http://pbs.twimg.com/ext_tw_video_thumb/",string.Empty);
+						
+						if(thupic[0]=='h')
+						{
+							thupic=thupic.Replace("http://pbs.twimg.com/amplify_video_thumb/","@");
+						}
+						
+						if(thupic[0]=='h')
+						{
+							thupic=thupic.Replace("http://pbs.twimg.com/tweet_video_thumb/","!");
+						}
+						
+						if(!deuu.Contains(thupic))
+						{
+							deuu.Add(thupic);
+							string vid=siu[3].Replace("https://video.twimg.com/ext_tw_video/",string.Empty);
+							
+							if(vid[0]=='h')
+							{
+								vid=vid.Replace("https://video.twimg.com/amplify_video/","@");
+							}
+							
+							if(vid[0]=='h')
+							{
+								vid=vid.Replace("https://video.twimg.com/tweet_video/","!");
+							}
+							
+							
+							string msg=html[i+2].Split(sepQuo)[1].Replace("https://twitter.com/",string.Empty).Replace("https://mobile.twitter.com/",string.Empty);
+							sw.WriteLine(thupic+"\t"+vid+"\t"+msg);
+							
+							getsome=true;
+						}
+					}
+				}
+				
+				if(getsome)
+				{
+					sw.Flush();
+				
+					if(deuu.Count>1000)
+					{
+						
+						sw.Close();
+						
+						klening();
+						
+						system("start eroneto.exe");
+						break;
+						
+					}
+				}
+				
+				Thread.Sleep(90000);
+			}
+		}
+		
+	}
+}
---
+++ keepsec/kero/prgg.cs
@@ -0,0 +1,98 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace eroneto
+{
+	
+	static class utily
+	{
+	
+		[DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+		public static extern int system(string command);
+		
+		static string[][] ReadAllLines(string path)
+		{
+			var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+			int fsl=(int)fs.Length;
+			byte[] fb=new byte[fsl];
+			fs.Read(fb,0,fsl);
+			fs.Close();
+			var data= Encoding.ASCII.GetString(fb).Replace("\r\n","\n").Split('\n');
+
+			int cota=data.Length-1;
+			string[][] reta=new string[cota][];
+			
+			for(int i=0;i<cota;i++)
+			{
+				 var ivo=data[i].Split('\t');
+				
+				 	
+				 reta[i]=ivo;
+				 
+			}
+			
+			
+			
+			return reta;
+
+		}
+		
+		static void mkjs()
+		{
+			system("rd /s /q zb");
+			system("md zb");
+			
+			string[][] data=ReadAllLines("rlines.csv");
+			int cota=data.Length-1;
+			string[] izkor=new string[4+cota*3];
+			string[] mizr=data[0];
+			izkor[0]="thumb=['";
+			izkor[1+cota]=mizr[0]+"'];\n\nvids=['";
+			izkor[2+cota*2]=mizr[1]+"'];\n\nmsgs=['";
+			izkor[3+cota*3]=mizr[2]+"'];";
+			
+			for(int i=1;i<cota;i++)
+			{
+				mizr=data[cota-i];
+				izkor[i]=mizr[0]+"',\n'";
+				izkor[1+cota+i]=mizr[1]+"',\n'";
+				izkor[2+i+cota*2]=mizr[2]+"',\n'";
+			}
+			
+			File.WriteAllText("aadata.js",string.Concat(izkor));
+		}
+		
+		static void cleandup()
+		{
+			 HashSet<string> deuuk = new HashSet<string>();
+			 string[] data=File.ReadAllLines("rlines.csv");
+			 int cota=data.Length;
+			 List<string> kle=new List<string>();
+			for(int i=0;i<cota;i++)
+			{
+				var ivo=data[i].Split('\t');
+				if(!deuuk.Contains(ivo[0]))
+				{
+					kle.Add(data[i]);
+					deuuk.Add(ivo[0]);
+				}
+
+			}
+			
+			File.WriteAllLines("rlines_.csv",kle.ToArray());
+		}
+		
+		static void Main(string[] args)
+		{
+			//cleandup();
+			mkjs();
+		}
+		
+	}
+}
