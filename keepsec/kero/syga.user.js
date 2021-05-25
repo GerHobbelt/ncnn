86a40e663060bee198d60d10d661949e7e6c67a8
Klanly <natural858@gmail.com>
2021-05-25 13:04:09 +0800

    Add files via upload

--- keepsec/kero/syga.user.js
+++ keepsec/kero/syga.user.js
@@ -9,19 +9,19 @@
 
 function settxbox()
 {
-txbx=document.getElementsByClassName('footer')[0];
+var txbx=document.getElementsByClassName('footer')[0];
 txbx.innerHTML='';
-yina = document.createElement('textarea');
+var yina = document.createElement('textarea');
 yina.cols=200;
 yina.rows=200;
 txbx.appendChild(yina);
 return yina;
 }
 
-var txbx=settxbox();
+window.txbx=settxbox();
 
-var twilistl=twilist.length;
-var curna=0;
+window.twilistl=twilist.length;
+window.curna=0;
 
 const vtwsig=	['',				'@',				'!',			'+'];
 const vtwimg=['https://pbs.twimg.com/ext_tw_video_thumb/',	'https://pbs.twimg.com/amplify_video_thumb/',	'https://pbs.twimg.com/tweet_video_thumb/','https://pbs.twimg.com/media/'];
@@ -30,7 +30,7 @@ const vtwvid=['https://video.twimg.com/ext_tw_video/',		'https://video.twimg.com
 function trimthumb(thupic)
 {
 
-	for(i=0;i<4;i++)
+	for(var i=0;i<4;i++)
 	{
 		thupic = thupic.replace(vtwimg[i], vtwsig[i]);
 		if (thupic.charAt(0) != 'h') {return thupic;}
@@ -40,7 +40,7 @@ function trimthumb(thupic)
 
 function trimvid(vid)
 {
-	for(i=0;i<3;i++)
+	for(var i=0;i<3;i++)
 	{
 		vid = vid.replace(vtwvid[i], vtwsig[i]);
 		if (vid.charAt(0) != 'h') {return vid;}
@@ -58,8 +58,8 @@ function reclog()
 if(vio)
 {
 	vio=vio.children[0].children[0];
-	ymg=vio.children[0];
-	txbx.value+='\n'+trimthumb(ymg.src)+'\t'+trimvid(vio.href)+'\t-'+twilist[curna-1];
+	var ymg=vio.children[0];
+	txbx.value+='\n'+trimthumb(ymg.src)+'\t'+trimvid(vio.href)+'\t^'+twilist[curna-1];
 	return;
 }
 txbx.value+='\n;'+twilist[curna-1];
@@ -91,14 +91,4 @@ function fioua()
 	}
 }
 
-var syko=setInterval(fioua, 80000);
-
-/*
-document.onkeydown=function(e) {
-    switch (e.keyCode) {
-case 104:
-fioua();
-break;
-}
-}
-*/
+window.syko=setInterval(fioua, 80000);
