888be8f8590b8dd0531213b6492968c36420336c
Klanly <natural858@gmail.com>
2021-07-05 23:26:28 +0800

    Update consolejs.js

--- keepsec/kero/consolejs.js
+++ keepsec/kero/consolejs.js
@@ -161,3 +161,304 @@ kole[i].src=uv.replace('/__rs_l30x30','');
 
 }
 
+//imh
+//https://imhentai.xxx/js/expp.js
+
+function klear()
+{
+    lding=0;
+    pglim=0;
+    pgnn=0;
+    pgnn2=0;
+    notload=false;
+    loadcot=0;
+    document.body.innerHTML='verysma<div>verysma</div>sma<div>sma</div>big<div>big</div><div>loading</div><pre>info1</pre><pre>info2</pre>';
+    
+    
+    
+    averysma=document.body.children[0];
+    asma=document.body.children[1];
+    abig=document.body.children[2];
+    aload=document.body.children[3];
+    ainfo1=document.body.children[4];
+
+    nwd=window.open('https://ezgif.com/optimize', null, null);
+
+
+}
+var block5s=false;
+
+function rst5s()
+{
+    block5s=false;
+
+}
+
+
+document.ondblclick=function(ev)
+{
+
+    if(block5s){return;}
+
+    var ele=ev.target.src;
+    if(ele)
+    {
+        nwd.location.href ='https://ezgif.com/optimize?url='+ele;
+        block5s=true;
+        setTimeout(rst5s, 5000);
+        nwd.focus();
+
+    }
+
+}
+
+function k3(clea)
+{
+
+
+    
+        k2(clea);
+   
+    
+    loadcot=0;
+    notload=false;
+}
+
+function findnone()
+{
+    ainfo1.innerText+="\n'"+this.alt+"',";
+    this.onload=null;
+    this.onerror=null;
+    this.remove();
+
+}
+
+function findnone2()
+{
+   lding--;
+    this.onload=null;
+    this.onerror=null;
+    if(this.naturalWidth==0)
+    {
+        this.remove();
+        
+    }
+
+}
+
+function findsome()
+{
+    abig.appendChild(this);
+    this.onload=null;
+    this.onerror=null;
+    //this.previousSibling.remove();
+
+}
+
+function k2(typ)
+{
+    switch(typ)
+    {
+        case 1:
+            asma.innerHTML='';
+            return;
+            
+        case 2:
+           averysma.innerHTML='';
+           return;
+
+        case 3:
+            asma.innerHTML='';
+            averysma.innerHTML='';
+            break;
+
+
+
+    }
+    abig.innerHTML='';
+
+
+}
+
+function findsome2()
+{
+
+    lding--;
+
+    var naw=this.naturalWidth;
+    if(naw==0)
+    {
+        this.remove();
+        return;
+    }
+    naw=(naw*this.naturalHeight)>>14;
+
+
+
+    
+    loadcot+=(10+naw);
+    if(loadcot>9999){
+        notload=true;
+        document.title='('+document.title+')';
+        }
+
+    
+    if(naw>30)
+    {
+        abig.appendChild(this);
+
+    }
+    else if(naw<5)
+    {
+        averysma.appendChild(this);
+    }
+    else
+    {
+        asma.appendChild(this);
+
+    }
+
+    this.onload=null;
+    this.onerror=null;
+
+    
+
+}
+
+function mkurl(orig)
+{
+
+    return 'https://m'+orig.replace('@','.imhentai.xxx/')+'/';
+}
+
+function pgcot10()
+{
+    if(notload){return;}
+
+    abig.innerHTML='';
+    var endo=pgnn+10;
+    for(pgnn=pgnn;pgnn<endo;pgnn++)
+    {
+        var ymg=document.createElement('img');
+        ymg.src=mkurl(gal[pgnn])+pglim+'t.jpg';
+        ymg.alt=gal[pgnn];
+        ymg.onload=findsome;
+        ymg.onerror=findnone;
+        aload.appendChild(ymg);
+
+    }
+
+
+}
+
+function getgoodpgnn(endo)
+{
+    for(var i=pgnn;i<pgE;pgnn++)
+    {
+        if(endo<gallims[pgnn]){return pgnn;}
+    }
+}
+
+function pgshow10()
+{
+    if(notload){return;}
+
+    if(lding>10){return;}
+
+    var endo=pgnn2+10;
+    pgnn=getgoodpgnn(endo);
+
+    document.title=pgnn+','+(pgnn2-1);
+
+    for(var i=pgnn2;i<endo;i++)
+    {
+        var ymg=document.createElement('img');
+        var syg=gal[pgnn];
+        ymg.alt=syg;
+        ymg.src=mkurl(syg)+i+'.gif';
+        
+        ymg.onload=findsome2;
+        ymg.onerror=findnone2;
+        aload.appendChild(ymg);
+        lding++;
+
+    }
+
+    
+    
+    pgnn++;
+    if(pgnn>=pgE)
+    {
+        pgnn=0;
+        pgnn2+=10;
+       
+    }
+    
+
+}
+
+var glolim=50;
+var gallims=null;
+
+
+function prepgal()
+{
+    gallims=new Array(pgE);
+    if(gal[0].split('@').length==3)
+    {
+        for(var i=0;i<pgE;i++)
+        {
+            var imz=gal[i].split('@');
+            gal[i]=imz[0]+'@'+imz[2];
+            gallims[i]=10+parseInt(imz[1].substr(1),10)*5;
+        }
+    }
+    else
+    {
+        for(var i=0;i<pgE;i++)
+        {
+            gallims[i]=glolim;
+        }
+    }
+}
+
+function  dofshow()
+{
+    pgE=gal.length;
+    prepgal();
+    pgshow10();
+    setInterval(pgshow10, 5000);
+
+}
+
+function f_show(galsta,pgsta,lymz)
+{
+    
+    pgnn=galsta;
+    pgnn2=pgsta+1;
+
+    if((typeof gal === 'undefined')&&lymz){
+        glolim=lymz*10;
+        var sk = document.createElement('script');
+	   sk.src='https://klanly.github.io/imh/az'+lymz+'.js';
+	   sk.onload=dofshow;
+	   document.body.appendChild(sk);
+    }
+    else{dofshow();}
+
+
+}
+
+
+function f_cot(lim)
+{
+    ainfo1.innerText='less than '+lim;
+    pglim=lim;
+    setInterval(pgcot10, 2000);
+
+}
+
+klear();
+
+
+//var gal=[
